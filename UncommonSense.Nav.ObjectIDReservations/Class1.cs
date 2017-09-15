edfc3c10ab3ea2d0c984fc2398d102682b611176
Jan Hoek <github@uncommonsense.nl>
2017-09-15 14:36:39 +0200

    Add project files.

---
+++ UncommonSense.Nav.ObjectIDReservations/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UncommonSense.Nav.ObjectIDReservations
+{
+    public class Class1
+    {
+    }
+}
