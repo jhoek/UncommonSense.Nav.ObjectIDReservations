c5e1c427f5dac6e5e562c590fbf7affbaaeca186
Jan Hoek <jhoek@users.noreply.github.com>
2017-12-06 11:57:21 +0100

    Delete test.ps1

--- test.ps1
+++
@@ -1,3 +0,0 @@
-﻿Import-Module C:\Users\jhoek\Desktop\UncommonSense.Nav.ObjectIDReservations\UncommonSense.Nav.ObjectIDReservations\bin\Debug\UncommonSense.Nav.ObjectIDReservations.dll -Force
-
-New-NavObjectIDReservation Table 55
